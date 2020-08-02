35bdbc24dd093dbb2ac3fd95f4327496fd1b3159
mark1227 <mark122787@gmail.com>
2020-08-02 23:34:55 +0800

    dynamic dashboard, preparing for routes and models

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,22 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/dashboard', function () {
-    return view('admindash');
+Route::get('/order', function () {
+    return view('order');
 });
+
+Route::get('/services', function () {
+    return view('services');
+});
+
+Route::get('/order', function () {
+    return view('order');
+});
+
+Route::get('/order', function () {
+    return view('order');
+});
+
+Route::post('/products', 'ProductsController@index')->name('orders.post');
+Route::get('/orders/{username}', 'OrdersController@index')->name('orders.show');
+Route::get('/profile/{username}', 'ProfileController@index')->name('profile.show');
